738eb9e3a05492d8936c009f84a035bd691ebb4a
samyamsapkt-web <samyamsapkt@gmail.com>
2026-03-23 20:27:22 -0400

    Delete SIaaS Software directory

--- SIaaS Software/backend/src/supabaseAdmin.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { config } from "./config.js";
-
-export const supabaseAdmin = createClient(config.supabaseUrl, config.supabaseServiceRoleKey, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false
-  }
-});
